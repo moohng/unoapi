6178ebb2f73ce6aef45c5e25b8417f0cd9a80e87
mohong <moohng@126.com>
2025-10-17 17:37:38 +0800

    fix: 修复 OpenAPI 文档获取错误处理，优化日志输出

--- packages/core/src/doc/index.ts
+++ packages/core/src/doc/index.ts
@@ -6,9 +6,10 @@ import { getDefaultCacheFile, loadConfig } from '../config/index.js';
  */
 export async function updateOpenAPIDoc() {
   console.log('更新 OpenAPI 文档');
+
   const config = await loadConfig();
   const jsonDoc = await (typeof config.openapiUrl === 'function' ?  config.openapiUrl() : fetchDoc(config.openapiUrl));
-  console.log('OpenAPI 文档获取成功', jsonDoc);
+  console.log('OpenAPI 文档获取成功');
 
   const cacheFile = config.cacheFile || getDefaultCacheFile(typeof config.output === 'string' ? config.output : config.output?.[0]);
   await writeFile(cacheFile, JSON.stringify(jsonDoc, null, 2));
@@ -17,5 +18,9 @@ export async function updateOpenAPIDoc() {
 
 async function fetchDoc(url: string) {
   const res = await fetch(url);
-  return await res.json();
+  const resJson = await res.json();
+  if (!res.ok) {
+    throw new Error(`无法获取 OpenAPI 文档：${res.status} ${res.statusText}，${resJson.message}`);
+  }
+  return resJson;
 }
