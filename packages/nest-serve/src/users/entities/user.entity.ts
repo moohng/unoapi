ea869a8ae9251800add051490ea73c70fecfeb21
mohong <moohng@126.com>
2025-10-28 14:34:27 +0800

    测试2

--- packages/nest-serve/src/users/entities/user.entity.ts
+++ packages/nest-serve/src/users/entities/user.entity.ts
@@ -1,5 +1,4 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { Cat } from 'src/cats/entities/cat.entity';
 
 export class User {
   @ApiProperty({ description: '用户ID' })
@@ -13,7 +12,4 @@ export class User {
 
   @ApiPropertyOptional({ description: '头像地址' })
   avatarUrl?: string;
-
-  @ApiPropertyOptional({ description: '用户的猫咪列表', type: [Cat] })
-  cats?: Cat[];
 }
